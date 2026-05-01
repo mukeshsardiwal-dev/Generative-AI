3441895c67321a22cd0c45a622bea202e95ca617
Mukesh Sardiwal <mukeshsardiwal.dev@gmail.com>
2026-05-01 14:57:54 +0530

    Inital Chatbot completed

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -1,14 +1,21 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+messages = [
 
-model = GoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.9)
+]
+
+model = ChatOpenAI(model="gpt-4o-mini",temperature=0.9)
 print("------------You are taking to AI Agent Created By Mukesh Sardiwal Type 0 to exit------------")
 while(True):
 
     prompt = input("You: ")
+    messages.append(prompt)
     if prompt == "0":
         break
-    response = model.invoke(prompt)
-    print("Agentic Bot:",response)
+    response = model.invoke(messages)
+    messages.append(response.content)
+    print("Agentic Bot:",response.conten)
+
+print(messages)
